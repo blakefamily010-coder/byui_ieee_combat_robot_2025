eb92c05fa2e726599902a75b3b080c40de24309e
blakefamily010-coder <blakefamily010@gmail.com>
2025-10-16 18:29:12 -0600

    add webpage

--- space_program/space_program.ino
+++ space_program/space_program.ino
@@ -1,6 +1,14 @@
 #include <WiFi.h>
+#include <index.h>
 
 WiFiServer server(80);
+const char WIFI_SSID[] = "space_program";
+const char WIFI_PASSWORD[] = "538976";
+
+void handleHome(WiFiClient& client, const String& method, const String& request, const QueryParams& params, const String& jsonData) {
+  server.sendResponse(client, HTML_CONTENT);
+}
+
 void setup() {
   // put your setup code here, to run once:
   // TODO: setup wifi
