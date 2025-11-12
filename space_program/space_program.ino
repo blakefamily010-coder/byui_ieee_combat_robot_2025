133cfbb26184d7eef50aaee05e1958b8264b8a07
etvance <131794089+etvance@users.noreply.github.com>
2025-11-11 18:40:22 -0700

    Update space_program.ino

--- space_program/space_program.ino
+++ space_program/space_program.ino
@@ -1,60 +1,21 @@
-#include <WiFi.h>
-#include <ESPAsyncWebServer.h>
-// #include <AsyncTCP.h>
 
-AsyncWebServer server(80);
+#include <Servo.h>
 
-const char WIFI_SSID[] = "space_program";
-const char WIFI_PASSWORD[] = "538976";
-const int test_pin = 21;
+Servo ESC;     // create servo object to control the ESC
 
-// NOTE: this is EXTREAMLY important, it should only be touched with good reason
-// ensure that the robot is OFF while it cannot recieve commands
-void disconect() {
-    // TODO: make the thing go OFF!
-    Serial.println("WIFI disconected");
-    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-    pinMode(test_pin, OUTPUT);
-}
+int JOY_Y;  // value from the analog pin
 
 void setup() {
-    // put your setup code here, to run once:
-    // TODO: setup wifi
-    // - connect to network
-    // - setup disconect callback
-    //     - stop if not connected
-    Serial.begin(115200);
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-    // TODO: we cant detect if a client disconects verry easily
-    // make shure that the client regularly polls the server.
-    //
-    // WiFi.onEvent(Disconect, ARDUINO_EVENT_WIFI_AP_DISCONNECTED)
-    //
-    server.on("/control/a_button", HTTP_GET, [](AsyncWebServerRequest* request){
-        Serial.println("A button rising edge");
-        a_button();
-        request->send(200, "text/plain", "Hello, world");
-        // request->send(204);
-    });
-    server.on("/control/a_button_off", HTTP_GET, [](AsyncWebServerRequest* request){
-        Serial.println("A button falling edge");
-        a_button_off();
-        request->send(200, "text/plain", "Hello, world");
-        // request->send(204);
-    });
-    // server.on("/control/l_stick", HTTP_GET, [](AsyncWebServerRequest* request){
-    //     if (request->hasParam("x") && (request->hasParam("y"))) {
-    //         float x = request->getParam("x")->value().toFloat();
-    //         float y = request->getParam("y")->value().toFloat();
-    //         Serial.printf("Left stick: x=%.2f, y=%.2f\n", x, y);
-    //         Serial.println("A button falling edge");
-    //         request->send(204);
-    //     }
-    // });
-
+    // Attach the ESC on pin 9
+    ESC.attach(9,1000,2000); // (pin, min pulse width, max pulse width in microseconds) 
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
+    weaponControl(JOY_Y);
+}
+
+void weaponControl(int joyValue) {
+
+    motorValue = map(joyValue, 0, 1023, 0, 180);   // scale it to use it with the servo library (value between 0 and 180)
+    ESC.write(motorValue);    // Send the signal to the ESC
 }
